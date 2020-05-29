a6f8668b1c95a8327fc6af6f111485a8ffb3f00b
jredden <johnredden@aol.com>
2020-05-28 17:29:51 -1000

    user def system refactor visit system 1

--- src/test/java/com/zenred/cosmos/service_rules_and_infrastructure/SystemSegmentsTest.java
+++ src/test/java/com/zenred/cosmos/service_rules_and_infrastructure/SystemSegmentsTest.java
@@ -28,13 +28,28 @@ public class SystemSegmentsTest {
 		SystemSegments systemSegments = new SystemSegments();
 		List<List<Integer>> masterList = systemSegments.builder();
 		Integer [] positions = new Integer[4];
+		Boolean first = Boolean.TRUE;
 		for(List<Integer> deatailList: masterList){
 			for(Integer idex = 0; idex < deatailList.size(); idex++){
 				positions[idex] = deatailList.get(idex);
 			}
 			List<UV_Instance> uvList = systemDao.readSystemSegment(positions[0], positions[1], positions[2], positions[3]);
-			logger.info("uvList length:"+uvList.size());
+			logger.info("seg"+positions[0]+"_"+positions[1]+"_"+positions[2]+"_"
+			+positions[3]+":"+"uvList length:"+uvList.size());
 			total += uvList.size();
+			if(first){
+				String set = "";
+				Integer set2 = 0;
+				for(Integer idex2 = 0; idex2 < uvList.size(); idex2++){
+					set += "u:"+uvList.get(idex2).getuDimension()+"v:"+uvList.get(idex2).getvDimension()+"___";
+					if(++set2  == 2){
+						set += ":::";
+						set2 = 0;
+					}
+				}
+				logger.info("FirstSet:"+set);
+				first= Boolean.FALSE;
+			}
 		}
 		logger.info("total:"+total);
 	}
